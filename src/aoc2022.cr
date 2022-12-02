2353fed5113252bcc09fef98043eda23fbc95276
Chris Watson <cawatson1993@gmail.com>
2022-12-01 20:56:09 -0700

    day one

---
+++ src/aoc2022.cr
@@ -0,0 +1,7 @@
+require "./aoc2022/day_one"
+
+puts "Day one, part one:"
+input = File.read("src/inputs/day_one.txt")
+puts "Answer: #{DayOne.part_one(input)}"
+puts "Day one, part two:"
+puts "Answer: #{DayOne.part_two(input)}"
---
+++ src/aoc2022/day_one.cr
@@ -0,0 +1,16 @@
+module DayOne
+  def self.get_totals(input : String)
+    elves = input.strip.split(/\n{2,}/)
+    elves.map do |elf|
+      elf.split('\n').map(&.to_i).sum
+    end
+  end
+
+  def self.part_one(input : String)
+    get_totals(input).sort.reverse[0]
+  end
+
+  def self.part_two(input : String)
+    get_totals(input).sort[-3..-1].sum
+  end
+end
