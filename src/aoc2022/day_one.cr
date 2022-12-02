698209f995a8ab093649e8f3578d5dee14035ffb
Chris Watson <cawatson1993@gmail.com>
2022-12-01 21:03:15 -0700

    updated day 1 code

--- src/aoc2022/day_one.cr
+++ src/aoc2022/day_one.cr
@@ -1,6 +1,6 @@
 module DayOne
   def self.get_totals(input : String)
-    elves = input.strip.split(/\n{2,}/)
+    elves = input.strip.split("\n\n")
     elves.map do |elf|
       elf.split('\n').map(&.to_i).sum
     end
@@ -11,6 +11,6 @@ module DayOne
   end
 
   def self.part_two(input : String)
-    get_totals(input).sort[-3..-1].sum
+    get_totals(input).sort.last(3).sum
   end
 end
