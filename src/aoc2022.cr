e6551861a69839d401f040f2f48625a37774c332
Chris Watson <cawatson1993@gmail.com>
2022-12-03 22:10:39 -0700

    use some macro magic to handle the days

--- src/aoc2022.cr
+++ src/aoc2022.cr
@@ -1,19 +1,12 @@
 require "./aoc2022/*"
 
-puts "Day one, part one:"
-input1 = File.read("src/inputs/day_one.txt")
-puts "Answer: #{DayOne.part_one(input1)}"
-puts "Day one, part two:"
-puts "Answer: #{DayOne.part_two(input1)}"
+DAYS = %w[one two three four]
+
+{% for day in DAYS %}
+puts "Day {{ day.id }}, part one:"
+input1 = File.read("src/inputs/day_{{ day.id }}.txt")
+puts "Answer: #{Day{{ day.capitalize.id }}.part_one(input1)}"
+puts "Day {{ day.id }}, part two:"
+puts "Answer: #{Day{{ day.capitalize.id }}.part_two(input1)}"
 puts
-puts "Day two, part one:"
-input2 = File.read("src/inputs/day_two.txt")
-puts "Answer: #{DayTwo.part_one(input2)}"
-puts "Day two, part two:"
-puts "Answer: #{DayTwo.part_two(input2)}"
-puts
-puts "Day three, part one:"
-input2 = File.read("src/inputs/day_three.txt")
-puts "Answer: #{DayThree.part_one(input2)}"
-puts "Day three, part two:"
-puts "Answer: #{DayThree.part_two(input2)}"
+{% end %}
